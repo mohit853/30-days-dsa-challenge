c4605dda5da0a08998cfe6e345c62608febb3cef
Mohit <106512863+mohit853@users.noreply.github.com>
2025-09-22 11:42:23 -0700

    added solution of hurdle race hacker rank problem

---
+++ hurdle-race-hackerrank.py
@@ -0,0 +1,55 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'climbingLeaderboard' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY ranked
+#  2. INTEGER_ARRAY player
+#
+
+def climbingLeaderboard(ranked, player):
+    # Write your code here
+    last = None
+    unique =[]
+    for rank in ranked:
+        if rank != last:
+            unique.append(rank)
+            last=rank
+    
+    i= len(unique)-1
+    res=[]
+    for each_player in player :
+        
+        while i>=0 and each_player >= unique[i] :
+            i-=1
+        res.append(i+2)
+    
+    return res
+    
+    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    ranked_count = int(input().strip())
+
+    ranked = list(map(int, input().rstrip().split()))
+
+    player_count = int(input().strip())
+
+    player = list(map(int, input().rstrip().split()))
+
+    result = climbingLeaderboard(ranked, player)
+
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
