26651fa5049ae6e16a6fad9df1443c2897b17b36
Mohit <106512863+mohit853@users.noreply.github.com>
2025-10-12 12:41:37 -0700

    solution to  longest_common_subsequence_leetcode

---
+++ longest_common_subsequence_leetcode.py
@@ -0,0 +1,20 @@
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        
+        ## n rows and m cols
+        n= len(text1)+1
+        m=len(text2)+1
+        dp = [[0]*m for _ in range(n)]  
+
+        for i in range(1,n):
+            for j in range(1,m):
+
+                if text1[i-1] == text2[j-1]:
+                    dp[i][j] = 1+ dp[i-1][j-1]
+                else:
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+        
+        return dp[n-1][m-1]
+
+
+        
