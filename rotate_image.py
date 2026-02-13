fea2722086650f0cc94ffd64567d3d87c346a1c4
Mohit <106512863+mohit853@users.noreply.github.com>
2026-02-12 23:58:07 -0700

    Add matrix rotation functionality
    Implement in-place rotation of a matrix by 90 degrees clockwise.

---
+++ rotate_image.py
@@ -0,0 +1,16 @@
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        
+        
+        rows = len(matrix)
+        cols = len(matrix[0])
+        
+        for i in range(rows):
+            for j in range(i+1,cols):
+                matrix[i][j] , matrix[j][i] = matrix[j][i] , matrix[i][j]
+                
+        for i in range(rows):
+            matrix[i].reverse()
