8e9d47ac03bbbe77ada9b6a6a281b2b0df8a9e0c
Mohit <106512863+mohit853@users.noreply.github.com>
2026-02-15 23:37:55 -0700

    Add sortedArrayToBST function
    Implement function to convert sorted array to BST.

---
+++ sortedArrayToBST.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def sortedArrayToBST(self, nums: List[int]) -> Optional[TreeNode]:
+        n = len(nums)
+
+
+        def build(lo,hi):
+            if lo > hi:
+                return None
+
+            mid = (lo+hi) //2
+            root = TreeNode(nums[mid])
+
+            root.left = build(lo,mid-1)
+            root.right = build(mid+1,hi)
+            return root
+
+        return build(0,n-1)        
+    
