62041e63c2efb59bf3391d839212455bf19fc56e
Mohit <106512863+mohit853@users.noreply.github.com>
2026-02-18 17:09:08 -0700

    Add twoSum method in Solution class
    Implement twoSum method to find indices of two numbers that add up to target in a sorted array.

---
+++ two_sum_ii_input_array_is_sorted.py
@@ -0,0 +1,19 @@
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        
+
+        i=0
+        j=len(numbers) -1
+    
+        while i < j :
+            sum= numbers[i] + numbers[j]
+            ## assume we find nums[0] +nums[n-1] == target
+            if sum == target :
+                return [i+1,j+1]
+            ## assume first number is neagtive
+            elif sum < target : 
+                i+=1
+            
+            else:
+                j-=1
+
